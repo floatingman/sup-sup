9acf90c73c291075a9e725e3d3a1e8a3fb2fb8bf
Daniel Newman <dwnewman78@gmail.com>
2019-12-13 23:43:11 -0600

    Broken homepage into components and styled menu items

---
+++ .history/src/components/directory/directory.component_20191213231222.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Directory extends React.Component {
+  constructor(){
+    super();
+    this.state ={
+      sections: []
+    }
+  }
+
+  render() {
+    return(
+      <div className="directory-menu"></div>
+    )
+  }
+}
