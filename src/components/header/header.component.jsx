51ec6a2e23c6280e1913e82e040bfc44c7f3131b
Daniel Newman <dwnewman78@gmail.com>
2020-01-20 02:26:11 -0600

    Added redux reducer for users

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,32 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React from 'react'
+import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
 
-import { auth } from '../../firebase/firebase.utils'
-import { ReactComponent as Logo } from '../../assets/crown.svg';
+import {auth} from '../../firebase/firebase.utils'
+import {ReactComponent as Logo} from '../../assets/crown.svg'
 
-import './header.styles.scss';
+import './header.styles.scss'
 
 const Header = ({currentUser}) => (
-  <div className='header'>
-  <Link className='logo-container' to="/">
-    <Logo className='logo'></Logo>
-  </Link>
-  <div className='options'>
-    <Link className='option' to='/shop'>
-      SHOP
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo"></Logo>
     </Link>
-    <Link className='option' to='/shop'>
-      CONTACT
-    </Link>
-    {
-      currentUser ?
-      <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-      :
-      <Link className='option' to='/signin'>SIGN IN</Link>
-    }
-  </div>
-
+    <div className="options">
+      <Link className="option" to="/shop">
+        SHOP
+      </Link>
+      <Link className="option" to="/shop">
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
+      )}
+    </div>
   </div>
 )
 
-export default Header
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header)
