857910d5599a7c8b1a199f08d6ba0027953f840b
Daniel Newman <dwnewman78@gmail.com>
2020-01-21 22:51:22 -0600

    Added local storage to app

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,16 @@
 import {createStore, applyMiddleware, compose} from 'redux'
+import {persistStore} from 'redux-persist'
 import logger from 'redux-logger'
 
 import rootReducer from './root-reducer'
 
 const middlewares = [logger]
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middlewares)))
+export const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(...middlewares)),
+)
 
-export default store
+export const persistor = persistStore(store)
